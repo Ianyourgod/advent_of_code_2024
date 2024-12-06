3fb58c852550c31dc2688c87323f77e6314725b5
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-12-06 14:21:53 -0600

    super super fast speedup (30min -> 15s)

--- day_6a/src/main.rs
+++ day_6a/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
-#[derive(Debug, Clone, Copy)]
+use std::collections::HashSet;
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Copy)]
 struct Guard {
     x: i32,
     y: i32,
@@ -10,12 +12,6 @@ struct Guard {
 #[derive(Debug, Clone)]
 struct Map {
     grid: Vec<Vec<i8>>,
-}
-
-struct PreviousPosition {
-    x: i32,
-    y: i32,
-    facing: i8,
 }
 
 fn generate_map(input: &str) -> (Map, Guard) {
@@ -42,7 +38,7 @@ fn generate_map(input: &str) -> (Map, Guard) {
     (Map { grid }, guard)
 }
 
-fn get_next_move(map: &Map, guard: &mut Guard, previous_positions: &mut Vec<PreviousPosition>) -> (bool, bool, bool) { // moved, out of range, loop
+fn get_next_move(map: &Map, guard: &mut Guard, previous_positions: &mut HashSet<Guard>) -> ((i32, i32), bool, bool, bool) { // moved, out of range, loop
     // check position in front of guard
     let (f_x, f_y) = match guard.facing {
         0 => (guard.x, guard.y - 1),
@@ -53,18 +49,20 @@ fn get_next_move(map: &Map, guard: &mut Guard, previous_positions: &mut Vec<Prev
     };
 
     if f_x >= map.grid[0].len() as i32 || f_y >= map.grid.len() as i32 || f_x < 0 || f_y < 0 {
-        return (false, false, false);
+        return ((-1,-1),false, false, false);
     }
 
-    if previous_positions.iter().any(|p| p.x == f_x && p.y == f_y && p.facing == guard.facing) {
-        return (false, false, true);
+    if previous_positions.contains(&guard) {
+        return ((-1,-1),false, false, true);
     }
+
+    previous_positions.insert(guard.clone());
 
     // check if position in front of guard is a wall
     let ret = if map.grid[f_y as usize][f_x as usize] == 1 {
         // turn right
         guard.facing = (guard.facing + 1) % 4;
-        (false, true, false)
+        ((-1,-1),false, true, false)
     } else {
         // move forward
         match guard.facing {
@@ -74,50 +72,78 @@ fn get_next_move(map: &Map, guard: &mut Guard, previous_positions: &mut Vec<Prev
             3 => guard.x -= 1,
             _ => panic!("Invalid facing value"),
         }
-        (true, true, false)
+        ((guard.x, guard.y),true, true, false)
     };
 
-    previous_positions.push(PreviousPosition { x: guard.x, y: guard.y, facing: guard.facing });
-
     ret
+}
+
+fn round(map: &mut Map, guard: &mut Guard, x:i32, y:i32) -> bool {
+    let prev_guard_info = (guard.x, guard.y, guard.facing);
+
+    if map.grid[y as usize][x as usize] == 1 {
+        return false;
+    }
+    if x == guard.x && y == guard.y {
+        return false;
+    }
+    map.grid[y as usize][x as usize] = 1;
+
+    let mut previous_positions = HashSet::new();
+
+    loop {
+        let (_, _, moved, in_loop) = get_next_move(&map, guard, &mut previous_positions);
+
+        if in_loop {
+            map.grid[y as usize][x as usize] = 0;
+            guard.x = prev_guard_info.0;
+            guard.y = prev_guard_info.1;
+            guard.facing = prev_guard_info.2;
+            return true;
+        }
+
+        if !moved {
+            map.grid[y as usize][x as usize] = 0;
+            guard.x = prev_guard_info.0;
+            guard.y = prev_guard_info.1;
+            guard.facing = prev_guard_info.2;
+            return false;
+        }
+    }
 }
 
 fn main() {
     // read input.txt
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let (map, mut guard) = generate_map(&input);
+    let (mut map, mut guard) = generate_map(&input);
+
+    // generate movement positions
+    let mut positions = Vec::new();
+    let mut guard_copy = guard.clone();
+    let mut unused = HashSet::new();
+    loop {
+        let (pos, _, not_at_end, _) = get_next_move(&map, &mut guard_copy, &mut unused);
+
+        if (pos.0, pos.1) != (-1, -1) {
+            positions.push((pos.0, pos.1));
+        }
+
+        if !not_at_end {
+            break;
+        }
+    }
 
     // move guard
     let mut loops = 0;
-    for y in 0..map.grid.len() {
-        for x in 0..map.grid[y].len() {
-            let mut new_map = map.clone();
-            let mut new_guard = guard.clone();
-
-            if new_map.grid[y][x] == 1 {
-                continue;
-            }
-            if x == guard.x as usize && y == guard.y as usize {
-                continue;
-            }
-            new_map.grid[y][x] = 1;
-
-            let mut previous_positions = Vec::new();
-
-            loop {
-                let (_, moved, in_loop) = get_next_move(&new_map, &mut new_guard, &mut previous_positions);
-
-                if in_loop {
-                    loops += 1;
-                    break;
-                }
-
-                if !moved {
-                    break;
-                }
-            }
+    //let mut iters = 0;
+    //let pos_len = positions.len();
+    for (x, y) in positions {
+        if round(&mut map, &mut guard, x, y) {
+            loops += 1;
         }
-        println!("loops: {}", loops);
+
+        //iters += 1;
+        //println!("round {}/{}", iters, pos_len);
     }
 
     println!("loops: {}", loops);
