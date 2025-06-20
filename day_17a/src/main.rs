6645897cf515464315d81fd769fd92def1bf241e
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2025-06-19 23:46:52 -0500

    day 17 solution

--- day_17a/src/main.rs
+++ day_17a/src/main.rs
@@ -1,255 +1,219 @@
+#![feature(iter_array_chunks)]
+
 #[derive(Debug, Clone)]
-struct Computer {
-    register_a: i64,
-    register_b: i64,
-    register_c: i64,
-
-    program_counter: usize,
-    program: Vec<char>,
-}
-
-impl Computer {
-    pub fn new(reg_a: i64, reg_b: i64, reg_c: i64, program: Vec<char>) -> Self {
-        Computer {
-            register_a: reg_a,
-            register_b: reg_b,
-            register_c: reg_c,
-            program_counter: 0,
-            program,
-        }
-    }
-
-    pub fn from_str(input: &str) -> Self {
-        let lines = input.lines().collect::<Vec<&str>>();
-
-        let reg_a: i64 = lines[0].split_whitespace().last().unwrap().parse().unwrap();
-        let reg_b: i64 = lines[1].split_whitespace().last().unwrap().parse().unwrap();
-        let reg_c: i64 = lines[2].split_whitespace().last().unwrap().parse().unwrap();
-
-        let program_unparsed = lines[4].split_whitespace().last().unwrap().split(',').collect::<Vec<&str>>();
-        let program = program_unparsed.iter().map(|x| x.chars().next().unwrap()).collect::<Vec<char>>();
-
-        Computer::new(reg_a, reg_b, reg_c, program)
-    }
-
-    pub fn run(&mut self) -> Vec<u8> {
-        let mut total_output = Vec::new();
-        while self.program_counter < self.program.len() {
-            let instruction = self.parse_instruction(self.program_counter as usize);
-            let (output, dont_add) = self.run_instruction(instruction);
-
-            if let Some(output) = output {
-                total_output.push(output);
-            }
-
-            if !dont_add {
-                self.program_counter += 2;
-            }
-        }
-
-        total_output
-    }
-
-    fn parse_combo(combo: char) -> Combo {
-        match combo {
-            '0' => Combo::Literal(0),
-            '1' => Combo::Literal(1),
-            '2' => Combo::Literal(2),
-            '3' => Combo::Literal(3),
-            '4' => Combo::RegA,
-            '5' => Combo::RegB,
-            '6' => Combo::RegC,
-            _ => panic!("Invalid combo: {}", combo),
-        }
-    }
-
-    fn parse_literal(literal: char) -> Literal {
-        let value = literal.to_digit(10).unwrap() as u8;
-        Literal { value }
-    }
-
-    fn parse_instruction(&self, index: usize) -> Instruction {
-        match self.program[index] {
-            '0' => {
-                // adv
-                let combo = Computer::parse_combo(self.program[index + 1]);
-                Instruction::Adv(combo)
-            }
-            '1' => {
-                // bxl
-                let literal = Computer::parse_literal(self.program[index+ 1]);
-                Instruction::Bxl(literal)
-            }
-            '2' => {
-                // bst
-                let combo = Computer::parse_combo(self.program[index+ 1]);
-                Instruction::Bst(combo)
-            }
-            '3' => {
-                // jnz
-                let literal = Computer::parse_literal(self.program[index+ 1]);
-                Instruction::Jnz(literal)
-            }
-            '4' => {
-                // bxc
-                Instruction::Bxc
-            }
-            '5' => {
-                // out
-                let combo = Computer::parse_combo(self.program[index+ 1]);
-                Instruction::Out(combo)
-            }
-            '6' => {
-                // bdv
-                let combo = Computer::parse_combo(self.program[index+ 1]);
-                Instruction::Bdv(combo)
-            }
-            '7' => {
-                // cdv
-                let combo = Computer::parse_combo(self.program[index+ 1]);
-                Instruction::Cdv(combo)
-            }
-            _ => panic!("Invalid instruction: {}", self.program[index]),
-        }
-    }
-
-    fn run_instruction(&mut self, instruction: Instruction) -> (Option<u8>, bool) {
-        match instruction {
-            Instruction::Adv(combo) => {
-                let value = 2_i64.pow(self.get_combo_value(combo) as u32);
-                self.register_a /= value;
-                (None, false)
-            }
-            Instruction::Bdv(combo) => {
-                let value = 2_i64.pow(self.get_combo_value(combo) as u32);
-                self.register_b = self.register_a / value;
-                (None, false)
-            }
-            Instruction::Cdv(combo) => {
-                let value = 2_i64.pow(self.get_combo_value(combo) as u32);
-                self.register_c = self.register_a / value;
-                (None, false)
-            }
-            Instruction::Bxl(literal) => {
-                // bitwise xor of reg b and literal
-                self.register_b ^= literal.value as i64;
-                (None, false)
-            }
-            Instruction::Bst(combo) => {
-                // bitwise and of reg b and combo
-                self.register_b = self.get_combo_value(combo) % 8;
-                (None, false)
-            }
-            Instruction::Jnz(literal) => {
-                if self.register_a != 0 {
-                    self.program_counter = literal.value as usize;
-                }
-                (None, self.register_a != 0)
-            }
-            Instruction::Bxc => {
-                self.register_b ^= self.register_c;
-                (None, false)
-            }
-            Instruction::Out(combo) => {
-                let value = self.get_combo_value(combo) % 8;
-                (Some(value as u8), false)
-            }
-        }
-    }
-
-    fn get_combo_value(&self, combo: Combo) -> i64 {
-        match combo {
-            Combo::Literal(lit) => lit as i64,
-            Combo::RegA => self.register_a,
-            Combo::RegB => self.register_b,
-            Combo::RegC => self.register_c,
-        }
-    }
-
-
-
-    fn run_in_reverse(&mut self, expected_output:)
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Instruction {
+pub enum Instruction {
     Adv(Combo),
     Bxl(Literal),
     Bst(Combo),
     Jnz(Literal),
-    Bxc,
+    Bxc(Literal),
     Out(Combo),
     Bdv(Combo),
     Cdv(Combo),
 }
 
+impl Instruction {
+    pub fn from_tuple(t: (u8, u8)) -> Self {
+        match t.0 {
+            0 => Self::Adv(Combo::from_u8(t.1)),
+            1 => Self::Bxl(t.1),
+            2 => Self::Bst(Combo::from_u8(t.1)),
+            3 => Self::Jnz(t.1),
+            4 => Self::Bxc(t.1),
+            5 => Self::Out(Combo::from_u8(t.1)),
+            6 => Self::Bdv(Combo::from_u8(t.1)),
+            7 => Self::Cdv(Combo::from_u8(t.1)),
+            _ => unreachable!()
+        }
+    }
+
+    pub fn to_num(&self) -> (u8, u8) {
+        match self {
+            Instruction::Adv(com) => (0, com.to_num()),
+            Instruction::Bxl(lit) => (1, *lit),
+            Instruction::Bst(com) => (2, com.to_num()),
+            Instruction::Jnz(lit) => (3, *lit),
+            Instruction::Bxc(l) => (4, *l), // hardcode ftw
+            Instruction::Out(com) => (5, com.to_num()),
+            Instruction::Bdv(com) => (6, com.to_num()),
+            Instruction::Cdv(com) => (7, com.to_num()),
+        }
+    }
+}
+
+type Literal = u8;
+
 #[derive(Debug, Clone, Copy)]
-enum Combo {
+pub enum Combo {
     Literal(u8),
     RegA,
     RegB,
-    RegC,
+    RegC
 }
 
-#[derive(Debug, Clone, Copy)]
-struct Literal {
-    value: u8,
+impl Combo {
+    pub fn from_u8(n: u8) -> Self {
+        match n {
+            0..=3 => Combo::Literal(n),
+            4 => Combo::RegA,
+            5 => Combo::RegB,
+            6 => Combo::RegC,
+            _ => unreachable!()
+        }
+    }
+
+    pub fn to_num(&self) -> u8 {
+        match self {
+            Combo::Literal(n) => *n,
+            Combo::RegA => 4,
+            Combo::RegB => 5,
+            Combo::RegC => 6,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Computer {
+    pub ip: usize,
+    pub reg_a: u64,
+    pub reg_b: u64,
+    pub reg_c: u64,
+}
+
+impl Computer {
+    pub fn new(
+        reg_a: u64,
+        reg_b: u64,
+        reg_c: u64,
+    ) -> Self {
+        Self {
+            ip: 0,
+            reg_a,
+            reg_b,
+            reg_c,
+        }
+    }
+
+    pub fn from_input(input: &str) -> (Self, Vec<Instruction>) {
+        let mut lines = input.lines();
+
+        let a_line = lines.next().unwrap();
+        let b_line = lines.next().unwrap();
+        let c_line = lines.next().unwrap();
+        lines.next().unwrap();
+        let program_line = lines.next().unwrap().split(": ").nth(1).unwrap();
+
+        let reg_a = a_line.split(": ").nth(1).unwrap().parse().unwrap();
+        let reg_b = b_line.split(": ").nth(1).unwrap().parse().unwrap();
+        let reg_c = c_line.split(": ").nth(1).unwrap().parse().unwrap();
+        let program = program_line.split(',').array_chunks::<2>().map(|[opcode, operand]| {
+            let opcode = opcode.parse().unwrap();
+            let operand = operand.parse().unwrap();
+            Instruction::from_tuple((opcode, operand))
+        }).collect();
+
+        (Self::new(reg_a, reg_b, reg_c), program)
+    }
+
+    pub fn run(&mut self, a: u64, instructions: &Vec<Instruction>) -> Vec<u8> {
+        let mut output = Vec::new();
+        self.ip = 0;
+        self.reg_a = a;
+        self.reg_b = 0;
+        self.reg_c = 0;
+
+        while let Some(instruction) = instructions.get(self.ip) {
+            match instruction {
+                Instruction::Adv(com) => {
+                    let denom = 2_u64.pow(self.execute_combo(com) as u32);
+                    self.reg_a = self.reg_a / denom;
+                }
+                Instruction::Bxl(lit) => {
+                    self.reg_b = self.reg_b ^ *lit as u64;
+                }
+                Instruction::Bst(com) => {
+                    self.reg_b = self.execute_combo(com) % 8;
+                }
+                Instruction::Jnz(lit) => {
+                    if self.reg_a != 0 {
+                        self.ip = *lit as usize;
+                        continue;
+                    }
+                }
+                Instruction::Bxc(_) => {
+                    self.reg_b = self.reg_b ^ self.reg_c;
+                }
+                Instruction::Out(com) => {
+                    output.push(self.execute_combo(com) as u8 % 8);
+                }
+                Instruction::Bdv(com) => {
+                    let denom = 2_u64.pow(self.execute_combo(com) as u32);
+                    self.reg_b = self.reg_a / denom;
+                }
+                Instruction::Cdv(com) => {
+                    let denom = 2_u64.pow(self.execute_combo(com) as u32);
+                    self.reg_c = self.reg_a / denom;
+                }
+            }
+            self.ip += 1;
+        }
+
+        output
+    }
+
+    fn execute_combo(&self, combo: &Combo) -> u64 {
+        match combo {
+            Combo::Literal(l) => *l as u64,
+            Combo::RegA => self.reg_a,
+            Combo::RegB => self.reg_b,
+            Combo::RegC => self.reg_c
+        }
+    }
 }
 
 fn main() {
-    // read input.txt
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let base_computer = Computer::from_str(&input);
+    let (mut computer, instructions) = Computer::from_input(&input);
+    let instrs_as_out = instructions.iter().map(|i|i.to_num()).flat_map(|(a, b)| [a, b]).collect::<Vec<_>>();
 
-    /*
-    while !found.0 {
-        // we're trying to find the value of a that makes the computer output its input (program)
-        // we're gonna use multi-threading to speed up the process
-        // to make it not super stupid, each thread will handle a range of values of a. if none of them find the answer, we'll start again from a higher starting value
-        let mut handlers = Vec::new();
-        
-        println!("Going from {} to {}", lowest_a, lowest_a + 800000);
+    //println!("{:?}", computer.run(678628631453, &instructions));
+    
+    let mut initial_valid = Vec::new();
+    for a in 0..1024_u64 {
+        if computer.run(a, &instructions)[0] == instrs_as_out[0] {
+            initial_valid.push(a);
+        }
+    }
 
-        for i in 0..8 {
-            let (tx, rx) = mpsc::channel();
-            let mut computer = base_computer.clone();
-            let start = lowest_a + i * 100000;
-            let end = start + (i+1) * 100000;
-            let finding = looking_for.clone();
-            std::thread::spawn(move || {
-                let mut found = false;
-                for a in start..end {
-                    computer.register_a = a;
-                    computer.program_counter = 0;
-                    computer.register_b = 0;
-                    computer.register_c = 0;
-                    let output = computer.run();
-                    let output_chars = output.split(',').collect::<String>().chars().collect::<Vec<char>>();
+    let mut valid = vec![initial_valid];
 
-                    if output_chars == finding {
-                        tx.send(Some((a, output))).unwrap();
-                        found = true;
+    let mut bit_length = 10;
+    for num_correct in 2..=16 {
+        valid.push(Vec::new());
+        let prev_valid = valid.get(num_correct-2).unwrap().clone();
+        for mut a in prev_valid {
+            for d_a in 0..8 {
+                a = a + (d_a * 2_u64.pow(bit_length));
+                let out = computer.run(a, &instructions);
+
+                if out.len()<num_correct {
+                    continue;
+                }
+
+                let mut is_valid = true;
+                for i in 0..num_correct {
+                    if out[i]!=instrs_as_out[i] {
+                        is_valid = false;
                         break;
                     }
                 }
-                if !found {
-                    tx.send(None).unwrap();
-                }
-            });
-            handlers.push(rx);
-        }
 
-        for handler in handlers {
-            match handler.recv().unwrap() {
-                Some((a, output)) => {
-                    found = (true, Some((a, output)));
-                    break;
+                if is_valid {
+                    valid.get_mut(num_correct-1).unwrap().push(a);
                 }
-                None => continue,
             }
         }
-
-        lowest_a += 800000;
+        bit_length+=3;
     }
-    */
+    println!("{:?}", valid.get(14).unwrap().get(0).unwrap());
 }
